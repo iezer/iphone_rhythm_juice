6e3b20e3b2d980ec1337927d6000cf8129871a43
Isaac Ezer <isaac.ezer@gmail.com>
2010-01-19 15:24:59 -0500

    First iphone app with time tracker

---
+++ HelloWorld_iPhone/Classes/TimeTrackerNode.m
@@ -0,0 +1,32 @@
+//
+//  TimeTrackerNode.m
+//  HelloWorld
+//
+//  Created by Isaac on 17/01/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import "TimeTrackerNode.h"
+
+
+@implementation TimeTrackerNode
+
+@synthesize mName;
+@synthesize mElapsed;
+@synthesize mStartDate;
+
+- (TimeTrackerNode*)initWithName:(NSString *)name elapsed:(NSTimeInterval)elapsed starting:(NSDate*)startDate {
+	self.mName = name;
+	self.mElapsed = elapsed;
+	self.mStartDate = startDate;
+	return self;
+}
+
+- (NSString*)description {
+	NSString* ret = [NSString stringWithFormat:@"%@ %@ elapsed:%1.2f",
+					 self.mName, 
+					 [self.mStartDate description], 
+					 self.mElapsed	];
+	return ret;
+}
+@end
